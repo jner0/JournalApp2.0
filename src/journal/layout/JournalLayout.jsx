609eef0a184c6a19525cf5bc11f39fe297e44110
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-05-23 00:06:24 -0500

    first commit

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material"
+import { NavBar } from "../components"
+
+const drawerWidth = 240;
+
+export const JournalLayout = ({children}) => {
+  return (
+    <Box sx={{display: 'flex'}}>
+        {/* NavBar */}
+        <NavBar drawerWidth={drawerWidth}/>
+        {/* SideBar */}
+
+
+        <Box 
+            component='main'
+            sx={{ flexGrow: 1, p: 2}}
+        >
+
+            {/* ToolBar */}
+
+            {children}
+
+        </Box>
+    </Box>
+  )
+}
---
+++ src/journal/pages/JournalPage.jsx
@@ -0,0 +1,14 @@
+import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
+
+
+export const JournalPage = () => {
+  return (
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed fugit rem maiores quidem voluptatem officia labore? Voluptates veritatis optio sapiente qui? Ducimus adipisci nam sequi sit corporis at nesciunt. Aliquid!
+      </Typography>
+    </JournalLayout>
+    
+  )
+}
