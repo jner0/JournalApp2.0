685d6e5b7cb05c01ea86d37b1b7caf551724ff38
Nemecio Rodriguez <jnemecio10@gmail.com>
2023-06-02 21:30:47 -0500

    Activando notas

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,10 +1,12 @@
 import { TurnedInNot } from "@mui/icons-material"
 import { Box, Divider, Drawer, Grid, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material"
 import { useSelector } from "react-redux"
+import { SideBarItem } from "./SideBarItem";
 
 export const SideBar = ({drawerWidth}) => {
 
     const { displayName } = useSelector( state => state.auth);
+    const { notes } = useSelector( state => state.journal);
 
   return (
     <Box
@@ -29,18 +31,8 @@ export const SideBar = ({drawerWidth}) => {
 
             <List>
                 {
-                    ['Enero', 'Febrero', 'Marzo'].map( text => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot/>
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={text}/>
-                                    <ListItemText secondary={'lorem sddasdasdasds sdfsdfsd dsfsdf dfsdfsdfs fsdfds'}/>
-                                </Grid>
-                            </ListItemButton>
-                        </ListItem>
+                    notes.map( note => (
+                        <SideBarItem key={note.id} { ...note }/>
                     ))
                 }
             </List>
